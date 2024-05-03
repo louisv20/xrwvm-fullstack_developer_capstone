a90b4b20f03ec5badad8d3accaa7aeaacee3029a
Luis <25castro25@gmail.com>
2024-05-03 19:24:43 -0400

    2nd section

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,9 +1,12 @@
+import Register from "./components/Register/Register"
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
     <Routes>
+      <Route path="/register" element={<Register />} />
       <Route path="/login" element={<LoginPanel />} />
     </Routes>
   );
