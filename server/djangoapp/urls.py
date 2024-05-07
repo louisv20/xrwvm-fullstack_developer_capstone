b1527adbbf0465e8a45efd76ec92db1cde9373d1
louisv20 <163933770+louisv20@users.noreply.github.com>
2024-05-07 07:32:53 -0700

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,18 +6,18 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
-    path(route='register', view=views.registration, name='register'),
+    # path for registration
+    path('register', views.registration, name='register'),
 
     # path for login
-    path(route='login', view=views.login_user, name='login'),
-    path(route='logout', view=views.logout_view, name='logout'),
-    path(route='get_cars', view=views.get_cars, name ='getcars'),
-    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
-    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
-    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
-    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
-    path(route='add_review', view=views.add_review, name='add_review'),
+    path('login', views.login_user, name='login'),
+    path('logout', views.logout_view, name='logout'),
+    path('get_cars', views.get_cars, name='getcars'),
+    path('get_dealers', views.get_dealerships, name='get_dealers'),
+    path('get_dealers/<str:state>', views.get_dealerships, name='get_dealers_by_state'),
+    path('dealer/<int:dealer_id>', views.get_dealer_details, name='dealer_details'),
+    path('reviews/dealer/<int:dealer_id>', views.get_dealer_reviews, name='dealer_details'),
+    path('add_review', views.add_review, name='add_review'),
 
     # path for dealer reviews view
 
